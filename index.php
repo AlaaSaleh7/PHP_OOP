1a522150e94686209b0d5421bd3a3521ed4bd66b
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-09-25 12:10:32 +0300

    Pulse Check Q3

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ class Fruit {
    public $firstFruit = "Apple";
    public $secondFruit = "Orange";
     //methods
-    public function getFruits (){
+    public function AllFruits (){
         return "All fruits"
     }
     public function favFruit(){
@@ -19,4 +19,24 @@ class Fruit {
 class Apple extends Fruit {
     public $color = "Green";
 }
+
+// 3. Create a new class make 
+//each property of it private and try to 
+//access it and change it using getters/ setters
+class Names {
+    private $one = "Alaa";
+    private $two = "Sarah";
+}
+// A. getters: a method which will get a private property from its class
+public function getName (){
+    return $this->one;
+}
+echo $one->getName();
+// B. setters: a method that will set or change the private property
+public function setName ($two){
+    return $this->two = $two; 
+}
+$two->setName("Ali");
+echo $two->getName();
+
 ?>
