0e8c1b44b97aeb4758f2ff21bf97377af7893705
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-09-25 12:25:38 +0300

    Pulse Check Q4 FinishAll

--- index.php
+++ index.php
@@ -4,39 +4,61 @@
 
 class Fruit {
     //Properties
-   public $firstFruit = "Apple";
-   public $secondFruit = "Orange";
+   public $NameFruit = "Apple";
+   public $colorFruit = "Yellow";
     //methods
-    public function AllFruits (){
-        return "All fruits"
+    public function myFruits (){
+        return "my fruits"
     }
     public function favFruit(){
-        return "$this->firstFruit the favorite fruits"
+        return "$this->NameFruit the favorite fruits"
     }
 }
 // 2.Create a class named Apple 
 //which inherits from the Fruit class add 1 additional property to it
 class Apple extends Fruit {
-    public $color = "Green";
+    public $priceFruit = "2 JD";
 }
 
 // 3. Create a new class make 
 //each property of it private and try to 
 //access it and change it using getters/ setters
 class Names {
-    private $one = "Alaa";
-    private $two = "Sarah";
+    private $firstName = "Alaa";
+    private $lastName = "Saleh";
 }
 // A. getters: a method which will get a private property from its class
 public function getName (){
-    return $this->one;
+    return $this->firstName;
 }
-echo $one->getName();
+echo $firstName->getName();
 // B. setters: a method that will set or change the private property
-public function setName ($two){
-    return $this->two = $two; 
+public function setName ($lastName){
+    return $this->lastName = $lastName; 
 }
-$two->setName("Ali");
-echo $two->getName();
+$lastName->setName("Mohammad");
+echo $lastName->getName();
+
+//4. Add a constructor to all the classes you have created
+class Fruit {
+    //Properties
+   public $NameFruit = "Apple";
+   public $colorFruit = "Yellow";
+    function __construct(){
+        $this->NameFruit = $NameFruit; 
+        $this->colorFruit = $colorFruit; 
+    }
+}
+$newFruit = new Fruit ("Grapes","Black");
+
+class Names {
+    private $firstName = "Alaa";
+    private $lastName = "Saleh";
+    function __construct (){
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+}
+$newOne = new Names ("Ahmad", "Saleh");
 
 ?>
